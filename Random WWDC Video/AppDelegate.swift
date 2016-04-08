b739616bf4b731e44489a6b339187a464687669e
Thomas Jeans <thomas.jeans@capitalone.com>
2016-04-08 17:23:43 -0400

    Initial commit

---
+++ Random WWDC Video/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Random WWDC Video
+//
+//  Created by Thomas Jeans on 4/8/16.
+//  Copyright © 2016 Thomas Jeans. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+}
+
